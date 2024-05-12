d5cf198e4d0b29b3a990c9f401c59ca6bfaaa1a2
nangsii <norbutsang.nangsa@gmail.com>
2024-05-13 00:05:31 +0200

    commiting stash

--- sorting.py
+++ sorting.py
@@ -4,7 +4,14 @@ my_list = [3, 6, 1, 9, 5]
 # input list: [3, 5]
 # output list: [5, 3]
 def my_swap(my_list):
-    pass
+    y = my_list[0]
+    my_list[0] = my_list[1]
+    my_list[1] = y
+    return my_list
+    
+swap_test = [3, 5]
+result_after_swap = my_swap(swap_test)
+print(result_after_swap)
 
 # 2. find the max element in a list 
 # output int:9
@@ -12,6 +19,16 @@ def my_max(my_list):
     my_list = [3, 9, 5]
 
         
+my_list = [3, 9, 5]
+
+maxnum = my_list[0]
+for x in my_list:
+  if x > maxnum:
+    maxnum = x
+print(maxnum)
+    
+   
+    
 
 
 # 3. find the nth max element in a list 
@@ -19,6 +36,16 @@ def my_max(my_list):
 # output int: 9
 # input list: [3, 9, 5] with nth_max = 2
 # output int: 5
+my_list = [3, 9, 5] 
+#2
+nth_max = my_list[0]
+for x in my_list:
+  if x > nth_max:
+    nth_max = x
+    
+print()
+
+
 def my_nth_max(my_list, nth_max):
     pass
 
