763d5c3c72310a61ed48d7681dfb30bb8f6cbd7b
nangsii <norbutsang.nangsa@gmail.com>
2024-05-19 22:03:19 +0200

    2nd max

--- sorting.py
+++ sorting.py
@@ -1,31 +1,35 @@
-my_list = [3, 6, 1, 9, 5]
+# # my_list = [3, 6, 1, 9, 5]
 
-# 1. create a function to swap two elements in a list
-# input list: [3, 5]
-# output list: [5, 3]
-def my_swap(my_list):
-    y = my_list[0]
-    my_list[0] = my_list[1]
-    my_list[1] = y
-    return my_list
+# # # 1. create a function to swap two elements in a list
+# # # input list: [3, 5]
+# # # output list: [5, 3]
+# def my_swap(my_list):
+#     y = my_list[0]
+#     my_list[0] = my_list[1]
+#     my_list[1] = y
+#     return my_list
     
-swap_test = [3, 5]
-result_after_swap = my_swap(swap_test)
-print(result_after_swap)
+# swap_test = [3, 5]
+# result_after_swap = my_swap(swap_test)
+# print(result_after_swap)
 
-# 2. find the max element in a list 
-# output int:9
-def my_max(my_list):
-    my_list = [3, 9, 5]
+# # # 2. find the max element in a list 
+# # # output int:9
+# # def my_max(my_list):
+# #     my_list = [3, 9, 5]
 
-        
-my_list = [3, 9, 5]
+     
+# def my_max(my_list):
+#   maxnum = my_list[0]  
+#   for x in my_list:
+#     if x > maxnum:
+#       maxnum = x
+#   return maxnum
 
-maxnum = my_list[0]
-for x in my_list:
-  if x > maxnum:
-    maxnum = x
-print(maxnum)
+
+# list_test = [3, 9, 5]
+# result_my_max = my_max(list_test)
+# print(result_my_max)
     
    
     
@@ -36,22 +40,44 @@ print(maxnum)
 # output int: 9
 # input list: [3, 9, 5] with nth_max = 2
 # output int: 5
-my_list = [3, 9, 5] 
-#2
-nth_max = my_list[0]
-for x in my_list:
-  if x > nth_max:
-    nth_max = x
-    
-print()
 
 
-def my_nth_max(my_list, nth_max):
-    pass
+def nth_max(my_list, n):
+    count = 0
+    # extend the life scope of the variable
+    nth_max
+    while count < n:
+        count = count + 1
+        nth_max = my_list[0]
+        for x in my_list:
+            if x > nth_max:
+                nth_max = x        
+        my_list.remove(nth_max)
+    return nth_max    
+            
+my_list = [3, -5, -2, -2000, -150] 
+n = 2
+result_nth_max = nth_max(my_list, n)
+print(result_nth_max)
+
+# option 2 move the nth_max to my_list[0]
+
+
 
 # 4. sort the list in ascending order
 # input list : [3, 6, 1, 9, 5]
 # output list: [1, 3, 5, 6, 9]
-def my_sort(my_list):
-    for x in my_list:
-        print(x)
+
+# def my_sort(my_list):
+#     max_num = my_list[0]
+#     for x in my_list:
+#         if x > max_num: 
+#             max_num = x 
+#     y = my_list[0]  
+#     my_list[0] = max_num
+#     my_list[max_num] = y
+#     return my_list
+        
+# test_list = [3, 6, 1, 9, 5]
+# result_my_sort =  my_sort(test_list)
+# print(result_my_sort)
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-count = 1
-# run loop till count is less than 5
-while count < 5:
-    print(count)
-    count = count + 1
+# count = 1
+# # run loop till count is less than 5
+# while count < 5:
+#     print(count)
+#     count = count + 1
+
+list = [3,5,6,7]
+list.remove(5)
+print(list)
